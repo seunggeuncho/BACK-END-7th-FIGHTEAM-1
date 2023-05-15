ff11daa7c5a75b94718ded55550d08d7361ea7f9
Jinse <akop4869@gmail.com>
2023-05-15 10:51:24 +0900

    Feat:teamspace close

--- src/main/java/com/example/fighteam/teamspace/domain/repository/TeamspaceRepository.java
+++ src/main/java/com/example/fighteam/teamspace/domain/repository/TeamspaceRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TeamspaceRepository extends JpaRepository<Attendance, Long> {
+public interface TeamspaceRepository extends JpaRepository<Teamspace, Long> {
 }
